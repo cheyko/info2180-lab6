0614f8a1c8d8368184adbba28cca0d694a2661bc
ariel wilson <ariel.wilson@mymona.uwi.edu>
2016-11-09 21:41:48 +0000

    it seems like i have finnaly come to grips with this whole git thing . anyways the ajax function was altered so that for each serch the require results would be retrieved.

--- click.js
+++ click.js
@@ -1,27 +1,38 @@
 var httpRequest;
+var word;
+var button;
 window.onload = function() {
-  document.getElementById("clickbutton").onclick = doAjax;
-  meaning();
-  
+  //document.getElementById("clickbutton").onclick = doAjax;
+  //meaning();
+  word =document.getElementsByTagName('input')[0];
+  button = document.getElementsByTagName('input')[1];
+
+	button.onclick =function() {
+		doAjax(); 
+		meaning();
+	}
 };
 
   function doAjax(){
-    httpRequest = new XMLHttpRequest();
-
+		httpRequest =new XMLHttpRequest();
     // GET Request
-    var url = "https://info2180-lab6-cheyko.c9users.io/request.php?q=definition";
+    //var url = "https://info2180-lab6-cheyko.c9users.io/request.php?q=definition";
+    var url = "request.php?q="+word.value;
     httpRequest.onreadystatechange = meaning;
     httpRequest.open('GET', url);
     httpRequest.send();
   }
 
   function meaning() {
+    var print = document.getElementById('result');
     if (httpRequest.readyState === httpRequest.DONE) {
       if (httpRequest.status === 200) {
         var response = httpRequest.responseText;
-        alert(response);
+        //alert(response);
+        print.innerHTML ='<h3> Result </h3>'+response;
       } else {
-        alert('There was a problem with the request.');
+        //alert('There was a problem with the request.');
+        print.innerHTML ='no response was found';
       }
     }
   }
